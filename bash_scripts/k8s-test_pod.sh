24f2d148dd8aee0829fd4ccc88a91aae2952f157
joeleavitt <joeleav1@gmail.com>
2023-01-18 14:21:59 -0500

    run port-forward in background

--- bash_scripts/k8s-test_pod.sh
+++ bash_scripts/k8s-test_pod.sh
@@ -2,4 +2,6 @@
 
 appPod=$(kubectl get pods -l app=hello-world --output=jsonpath={.items..metadata.name})
 
-kubectl port-forward $appPod 3000:3000
+kubectl port-forward $appPod 3000:3000 &
+
+curl localhost:3000
