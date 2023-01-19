bbee1d6924986884be14717c2131daf1a12caf5a
joeleavitt <joeleav1@gmail.com>
2023-01-19 01:40:47 -0500

    improve format

--- bash_scripts/docker-build_and_test.sh
+++ bash_scripts/docker-build_and_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Build the docker image and tag it
-echo "Building Image"
+echo "Building Image:"
 docker build -t joeleav/hello-world:0.1.0 .
 
 # Run the container in detached mode using the created image
-echo -e "\nRunning Container"
+echo -e "\nRunning Container:"
 docker run --name hello-world-release -d -p 3000:3000 joeleav/hello-world:0.1.0
 
 # Test a request to the container
-echo -e "\nMaking Request\n"
-curl localhost:3000
+echo -e "\nMaking Request:"
+curl -s localhost:3000
