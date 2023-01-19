a7302e25b195033a30904279577404774720c4e5
Joseph Leavitt <joeleav1@gmail.com>
2023-01-18 22:00:45 -0500

    Update k8s-test_pod_for_action.sh

--- bash_scripts/k8s-test_pod_for_action.sh
+++ bash_scripts/k8s-test_pod_for_action.sh
@@ -2,7 +2,7 @@
 
 appPod=$(kubectl get pods -n default -l app=hello-world --output=jsonpath={.items..metadata.name})
 
-kubectl port-forward $appPod 3000:3000 &
+kubectl port-forward $appPod :3000 &
 
 sleep 10
 
